d884550b9e6196fd0f59042f8d2818c37868283a
Phalla <55537405+phalla-doll@users.noreply.github.com>
2026-03-06 15:16:15 +0700

    feat: Suppress hydration warning in root layout
    Addresses hydration warnings by adding `suppressHydrationWarning` to the `html` and `body` tags in the root layout. This is a common practice in Next.js applications, especially when dealing with dynamic content or server-side rendering.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&display=swap" rel="stylesheet" />
       </head>
